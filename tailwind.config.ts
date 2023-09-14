8fa657564a9916aecd622a5a67f478bff3c5ea6e
Chathuranga Lakmal <chathudreamshms@gmail.com>
2023-09-14 21:02:15 +0530

    Make correction in tailwind.config.ts file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
-import type { Config } from 'tailwindcss'
+import type {Config} from 'tailwindcss'
 
 const config: Config = {
-  content: ['./src/app/**/*.{js,ts,jsx,tsx}',],
-  theme: { },
-  plugins: [],
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {},
+    plugins: [],
 }
 export default config
